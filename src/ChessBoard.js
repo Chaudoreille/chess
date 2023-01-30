55ffb03ad77f88376efb66ad5c1b225ca77fd10b
Chaudoreille <a.chaudoreille@gmail.com>
2023-01-30 12:55:39 +0100

    remove grid from ChessBoard

--- src/ChessBoard.js
+++ src/ChessBoard.js
@@ -2,24 +2,11 @@ class ChessBoard {
     constructor() {
         this.blackPieces = []
         this.whitePieces = []
-        this.initBoard()
         this.turn = false
-        
     }
 
     freeSquare(square) {
         return true
-    }
-
-    initBoard() {
-        // let board = []
-        // for (let i = 0; i < 8; i++) {
-        //     board[i] = []
-        //     for (let j = 0; j < 8; j++) {
-        //         board[i][j] = null;
-        //     }
-        // }
-        // this.board = board
     }
 }
 
