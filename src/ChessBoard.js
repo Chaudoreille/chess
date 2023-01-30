249f53760e88d970ce9f1e805b07d19ab66f53cf
Chaudoreille <a.chaudoreille@gmail.com>
2023-01-30 15:46:53 +0100

    merge with tests / add rook class

--- src/ChessBoard.js
+++ src/ChessBoard.js
@@ -3,6 +3,7 @@ class ChessBoard {
         this.blackPieces = []
         this.whitePieces = []
         this.turn = false
+        this.collisions = initCollisionBoard()
     }
 
     freeSquare(square) {
@@ -10,3 +11,18 @@ class ChessBoard {
     }
 }
 
+/*
+ * creates a null - filled 8 * 8 matrix 
+ */ 
+function initCollisionBoard() {
+    const board = []
+
+    for (let i = 0; i < 8; i++) {
+        board.push([])
+
+        for (let j = 0; j < 8; j++) {
+            board[i].push(null)
+        }
+    }
+    return board
+}
--- src/chess-pieces/Rook.js
+++ src/chess-pieces/Rook.js
@@ -1,13 +1,9 @@
 class Rook extends ChessPiece {
     legalMoves() {
         const legalMoves = []
-        for (let i = 0; i < 8; i++) {
-            if (i !== this.y) {
-                legalMoves.push(new Square(i, this.y))
-            }
-            if (i !== this.x) {
-                legalMoves.push(new Square(this.x, i))
-            }
+
+        for (let i = thix.x; i >= 0; i--) {
+            
         }
     }
 }
