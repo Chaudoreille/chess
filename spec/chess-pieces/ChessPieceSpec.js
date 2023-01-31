fbd46a5220b984afe0dab061c8b37314b99bc515
Chaudoreille <a.chaudoreille@gmail.com>
2023-01-31 11:11:28 +0100

    create all base tests for chessPieces, with mockups
    WIP - some tests still need implementation

--- spec/chess-pieces/ChessPieceSpec.js
+++ spec/chess-pieces/ChessPieceSpec.js
@@ -1,10 +1,17 @@
-let chessBoard, chessPiece, position
+let chessBoard, chessPiece, friend, foe
+const  position = mockSquare(1, 2, "B3")
+const positionFriend = mockSquare(0, 2, "A3")
+const positionFoe = mockSquare(1, 3, "B4")
+const color = BLACK
+const opposingColor = WHITE
 
 describe('ChessPiece', function() {
     beforeEach(() => {
-        chessBoard = mockChessBoard();
-        position = mockSquare(1, 2, "F2")
-        chessPiece = new ChessPiece(chessBoard, WHITE, position)
+        chessBoard = mockChessBoard()
+        chessPiece = new ChessPiece(chessBoard, color, position)
+        friend = new ChessPiece(chessBoard, color, positionFriend)
+        foe = new ChessPiece(chessBoard, opposingColor, positionFoe)
+        chessBoard.populate([chessPiece, friend, foe])
     });
 
     it('should be declared', () => {
@@ -12,24 +19,57 @@ describe('ChessPiece', function() {
     });
     
     describe('constructor method', () => {
-        it('should receive `chessBoard` and `square` as a parameter and store them in it\'s own properties', () => {
-            console.log(position, chessPiece.pos)
+        it('should receive `chessBoard`, `color` and `square` as a parameter and store them in it\'s own properties', () => {
             expect(chessPiece.board).toBe(chessBoard);
+            expect(chessPiece.color).toBe(color);
             expect(chessPiece.pos).toBe(position);
         });
     });
 
     describe('targets method', () => {
+        it("should be declared", function() {
+            expect(typeof chessPiece.targets).toEqual("function")
+        });
         it("should return false", function() {
-            let otherPiece = new ChessPiece()
-            expect(chessPiece.targets(otherPiece)).toEqual(false)
+            expect(chessPiece.targets(foe)).toEqual(false)
         });
     });
 
     describe('move method', () => {
-        it("should throw an Illegal Move error if the piece can't move to the new position", function() {
+        it("should be declared", function() {
+            expect(typeof chessPiece.move).toEqual("function")
+        });
+
+        it("should throw an error when the piece can't move to the new position", function() {
             let position2 = mockSquare(2, 2, "F3")
             expect(() => chessPiece.move(position2)).toThrow()
+        });
+
+        it("should throw an error when the piece tries to take a piece of the same color.", function() {
+        });
+
+        it("should move the piece on the chessBoard", function () {
+        });
+
+        it("should return the taken piece when it moves to a square occupied by an opposing piece", function() {
+        });
+    });
+
+    describe('pushMoveIfLegal method', () => {
+        it("should be declared", function() {
+            expect(typeof chessPiece.move).toEqual("function")
+        });
+
+        it(`should return true and push the move in legalMoves
+            when there is no chess piece on the destination square`, function () {
+        });
+
+        it(`should return false and not push the move in legalMoves
+            when there is a piece of the same color on the destination square`, function() {
+        });
+
+        it(`should return false and push the move in legalMoves
+            when there is a chess piece of the opposing color on the destination square`, function() {
         });
     });
 });  
