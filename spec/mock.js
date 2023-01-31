3a812a7d2a4d3e05f22f1a41baf0e566418b0921
Chaudoreille <a.chaudoreille@gmail.com>
2023-01-31 11:08:51 +0100

    rework on mockups
    - add mockups for base classes
    - add structured comments
    - some rework to match modifications on mocked classes

--- spec/mock.js
+++ spec/mock.js
@@ -4,9 +4,14 @@
  */
 const mockChessBoard = () => {
     return {
-        kings: [],
-        blackPieces: [],
-        whitePieces: [],
+        kings: {
+            [WHITE]: null,
+            [BLACK]: null,
+        },
+        pieces: {
+            [WHITE]: [],
+            [BLACK]: [],
+        },
         turn: WHITE,
         collisions: [
             [null, null, null, null, null, null, null, null],
@@ -19,13 +24,48 @@ const mockChessBoard = () => {
             [null, null, null, null, null, null, null, null]
         ],
         freeSquare: () => true,
+        populate: function(pieces) {
+            pieces.forEach((piece) => {
+                if (piece.type && piece.type == KING) {
+                    this.kings[piece.color] = piece
+                } else {
+                    this.pieces[piece.color].push(piece)
+                }
+                this.collisions[piece.pos.x][piece.pos.y] = piece
+            })
+        },
     }
 }
 
+/**
+ *
+ * @param {Integer} x
+ * @param {Integer} y
+ * @param {String} name the square name (A1 for <0,0> to H8 for <7,7>)
+ * @returns a Square mockup
+ */
 const mockSquare = (x, y, name) => {
     return {
         "x": x,
         "y": y,
-        "name": name
+        "name": name,
+    }
+}
+
+/**
+ *
+ * @param {*} chessBoard
+ * @param {*} color
+ * @param {*} square
+ * @returns a ChessPiece mockup
+ */
+const mockChessPiece = (chessBoard, color, square) => {
+    return {
+        board: chessBoard,
+        pos: square,
+        "color": color,
+        legalMoves: [],
+        targets: () => false,
+        move: () => false,
     }
 }
