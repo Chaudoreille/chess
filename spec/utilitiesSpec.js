3bdd7fa10c4256015a10f0691e7b0330abf013c0
Chaudoreille <a.chaudoreille@gmail.com>
2023-01-31 16:01:02 +0100

    add pushIfInBounds utility function + tests

--- spec/utilitiesSpec.js
+++ spec/utilitiesSpec.js
@@ -1,10 +1,10 @@
 describe('utility', function() {
-    const lowerPosition = {x: 0, y:0}
-    const higherXPosition = {x: 1, y:0}
-    const higherYPosition = {x: 0, y:1}
-    
-        describe('cmpPositions function', () => {
-        it('should be declared', () => {
+    describe('cmpPositions function', () => {
+            const lowerPosition = {x: 0, y:0}
+            const higherXPosition = {x: 1, y:0}
+            const higherYPosition = {x: 0, y:1}
+            
+                it('should be declared', () => {
             expect(typeof cmpPositions).toBe('function');
         });
         it('should return 0 when a.x == b.y and a.x == b.x', () => {
@@ -26,4 +26,42 @@ describe('utility', function() {
             expect(cmpPositions(higherYPosition, lowerPosition) > 0).toEqual(true)
         });
     });
+
+    describe('pushIfInBounds function', () => {
+        let container, position
+
+        beforeEach(() => {
+            container = []
+        });
+        it('should be declared', () => {
+            expect(typeof pushIfInBounds).toBe('function');
+        });
+        it('should not push position in container if position is out of bounds', () => {
+            position = mockSquare(-1, 7)
+            pushIfInBounds(container, position);
+            expect(container.length).toEqual(0)
+
+            position = mockSquare(8, 0)
+            pushIfInBounds(container, position);
+            expect(container.length).toEqual(0)
+
+            position = mockSquare(7, -1)
+            pushIfInBounds(container, position);
+            expect(container.length).toEqual(0)
+
+            position = mockSquare(0, 8)
+            pushIfInBounds(container, position);
+            expect(container.length).toEqual(0)
+        });
+        it('should push position in container if position is within bounds', () => {
+            position = mockSquare(0,7)
+            pushIfInBounds(container, position);
+            expect(container[0]).toEqual(position)
+
+            position = mockSquare(7,0)
+            pushIfInBounds(container, position);
+            expect(container[container.length-1]).toEqual(position)
+        });
+    });
+
 });  
--- src/utilities.js
+++ src/utilities.js
@@ -4,3 +4,10 @@ function cmpPositions(a, b) {
     }
     return a.x - b.x
 }
+
+function pushIfInBounds(container, position) {
+    if (position.x >= 0 && position.x < 8 && 
+        position.y >= 0 && position.y < 8) {
+        container.push(position)
+    }
+}
