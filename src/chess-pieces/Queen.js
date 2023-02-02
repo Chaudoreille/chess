ef9df4393b6a9596b307aeb42f245dd8da577f7b
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-02 01:49:18 +0100

    Queen checkbreaker done

--- src/chess-pieces/Queen.js
+++ src/chess-pieces/Queen.js
@@ -1,5 +1,7 @@
+import Square from "../Square.js"
 import ChessPiece from "./ChessPiece.js"
 import { QUEEN } from "../constants.js"
+import { oppositeColor, cmpPositions } from "../utilities.js"
 
 class Queen extends ChessPiece {
     constructor(chessBoard, color, square) {
@@ -19,6 +21,34 @@ class Queen extends ChessPiece {
         this.updateBottomLeft()
         this.updateBottomRight()
         this.breakChecks()
+    }
+
+    checkBreakerMoves() {
+        super.checkBreakerMoves()
+        const king = this.board.kings[oppositeColor(this.color)]
+        const positions = [king.pos, this.pos]
+        positions.sort(cmpPositions)
+
+        if (positions[0].x === positions[1].x) {
+            for (let i = positions[0].y+1; i < positions[1].y; i++) {
+                this.checkBreakers.push(new Square(positions[0].x, i))
+            }
+        } else {
+            for (let i = positions[0].x+1; i < positions[1].x; i++) {
+                this.checkBreakers.push(new Square(i, positions[0].y))
+            }
+        }
+
+        const inc_x = (king.pos.x - this.pos.x) / Math.abs(king.pos.x - this.pos.x)
+        const inc_y = (king.pos.y - this.pos.y) / Math.abs(king.pos.y - this.pos.y)
+        let x = this.pos.x + inc_x
+        let y = this.pos.y + inc_y
+
+        while (x !== king.pos.x && y !== king.pos.y) {
+            this.checkBreakers.push(new Square(x,y))
+            x += inc_x
+            y += inc_y
+        }
     }
 
     updateLeft() {
